669e59b3e94e3d4f2728d97603ae58c2ddf67148
AboJad <tornido.maroc@gmail.com>
2026-04-19 00:21:17 +0100

    Create vulnerable-example.js

---
+++ test/vulnerable-example.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'testdb'
+});
+
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  const query = `SELECT * FROM users WHERE id = ${userId}`;
+  
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.get('/search', (req, res) => {
+  const name = req.query.name;
+  const sql = "SELECT * FROM products WHERE name = '" + name + "'";
+  
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.listen(3000);
